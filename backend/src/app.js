c50102fd6b353aeb7dff2d265c72b8a1da735954
John Mwaura <muirujohn987@gmail.com>
2026-08-31 15:09:01 +0300

    Postgresql Integration

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+app.use('/api/auth', authRoutes);
+const express = require("express");
+const cors = require("cors");
+
+const healthRoutes = require("./routes/health.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/health", healthRoutes);
+
+module.exports = app;
